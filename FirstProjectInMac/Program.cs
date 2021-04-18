1729aa80976694d0164fee3bcfeb89d608f6ba69
suvgope <suvradipgope@gmail.com>
2021-04-18 00:40:39 -0400

    First Commit

---
+++ FirstProjectInMac/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstProjectInMac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press ENTER to exit ...");
+            Console.ReadLine();
+        }
+    }
+}
